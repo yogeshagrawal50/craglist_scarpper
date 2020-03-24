a07ab4225ccf175e3376324fbe62d9002829cb3d
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2020-03-24 18:03:48 +0530

    1st commit

---
+++ craglist_scarpper/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for craglist_scarpper project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'craglist_scarpper.settings')
+
+application = get_asgi_application()
